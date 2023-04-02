145fd63cadb9f70936bdfb314587d5a9d5f4509f
Robyn <robynainsley2i@gmail.com>
2023-04-02 23:17:38 +0200

    Reorganised files

---
+++ IWA1/IWA1/script.js
@@ -0,0 +1,36 @@
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 5;
+
+const number = document.querySelector('[data-key="number"]');//number that is to be changed when buttons are clicked
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
+
+const subtractHandler = () =>{
+    const newValue = parseInt(number.value) - STEP_AMOUNT;
+    number.value = newValue;
+
+    if(add.disabled === true){
+        add.disabled = false;
+    }
+
+    if(newValue <= MIN_NUMBER){
+        subtract.disabled = true;
+    }
+}
+
+const addHandler = () =>{
+    const newValue = parseInt(number.value) + STEP_AMOUNT;
+    number.value = newValue;
+
+    if(subtract.disabled === true){
+        subtract.disabled = false;
+    }
+    
+    if(newValue >= MAX_NUMBER){
+        add.disabled = true;
+    }
+}
+
+add.addEventListener('click', addHandler);
+subtract.addEventListener('click', subtractHandler);
---
+++ IWA3/Challenge-2/scripts.js
@@ -0,0 +1,5 @@
+import { role } from './nwabisa.js';
+import { role } from './johannes.js';
+import { role } from './alex.js';
+
+console.log('Roles:', nwabisa.role, johannes.role, alex.role)
