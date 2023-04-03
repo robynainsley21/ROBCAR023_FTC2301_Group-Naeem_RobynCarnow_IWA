c97bfc21360e3fb168d574f6ce5e487b01b8d269
Robyn <robynainsley2i@gmail.com>
2023-04-03 22:14:32 +0200

    Added challenge 2 folder and files

--- IWA6/Challenge-1/script.js
+++ IWA6/Challenge-1/script.js
@@ -14,13 +14,16 @@ console.log('Secondary phone is valid numerical string:', secondaryValid )
 
 
 // MY CODE
+//primary validation should fail (resolving to false, however since the secondary number is correct it should pass (resolving to true).
 const primaryPhone = 'O748105141'
 const secondaryPhone = '0219131568'
 
 // Only change below this line
+let number = '12'
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+
+const primaryValid = typeof number === typeof primaryPhone 
+const secondaryValid = typeof number === typeof secondaryPhone
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
