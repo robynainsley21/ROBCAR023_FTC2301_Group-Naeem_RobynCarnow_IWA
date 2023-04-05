f8876b8b6838498a8e596082c6a90edc7ed72eba
Robyn <robynainsley2i@gmail.com>
2023-04-05 05:40:07 +0200

    Reformatted code

--- IWA6/Challenge-2/script.js
+++ IWA6/Challenge-2/script.js
@@ -29,21 +29,17 @@ const minuteOfDay = 00;
 
 // Only change below this line
 
-if(hourOfDay === 00 && minuteOfDay === 00){
-	const taxAsDecimal = tax / '100'
-	const startingAfterTax = salary - taxAsDecimal
-	const cost = rent + tax + food
-	const balance = startingAfterTax - cost
-	console.log('R' + balance.toFixed(2))
-}else{
-	
+const calcBalance = () => {
+	if(hourOfDay === 00 && minuteOfDay === 00){
+		parseInt(tax)
+		console.log(tax)
+		const taxAsDecimal = tax / 100
+		const startingAfterTax = salary - taxAsDecimal
+		const cost = rent + tax + food
+		const balance = startingAfterTax - cost		
+		console.log('R' + balance.toFixed(2))
+
+	}	
 }
 
-// if (hourOfDay == 00 && minuteOfDay == 00){
-// 	parseInt(tax)
-// 	const taxAsDecimal = tax / '100';
-//     const afterTax = salary - taxAsDecimal
-// 	const balance = starting - transport - food - rent
-// }
-
-// (hourOfDay && minuteOfDay !== null)
+calcBalance()
