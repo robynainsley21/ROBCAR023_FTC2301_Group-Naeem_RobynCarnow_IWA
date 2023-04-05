a0c7854268b1d05893c16b19d5874a81a9c28806
Robyn <robynainsley2i@gmail.com>
2023-04-05 08:17:58 +0200

    Added and edited challenge 2

--- IWA7/Challenge-2/script.js
+++ IWA7/Challenge-2/script.js
@@ -1,4 +1,4 @@
-// TEMPLATE
+01111111111// TEMPLATE
 /*
 const nickname= "Timmy";
 const firstname = "Timothy";
@@ -8,3 +8,22 @@ console.log("Good Morning, ${nickname} || {firstname}!")
 
 // MY CODE
 
+//why original code did not work: quotes should not be used with template literals, instead backticks should be used; missing $ before nickname var
+
+const nickname = 'Timmy'
+const firstname = 'Timothy'     
+
+if(nickname){
+    console.log(`Good morning, ${nickname}!`)
+}else{
+    console.log('Good morning!')
+}
+
+// const nickname = undefined
+// const firstname = undefined     
+
+// if(nickname){
+//     console.log(`Good morning, ${nickname}!`)
+// }else if(nickname && firstname === undefined){
+//     console.log('Good morning!')
+// }
