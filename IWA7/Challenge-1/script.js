bb663b89bd0448d938ce351757ceaaedb22d40d1
Robyn <robynainsley2i@gmail.com>
2023-04-05 07:22:47 +0200

    Added and edited challenge 1

---
+++ IWA7/Challenge-1/script.js
@@ -0,0 +1,16 @@
+// TEMPLATE
+/*
+const value = "3"
+console.log(value + 4 + value)
+*/
+
+/*
+Why was '343' logged initially? 
+variable 'value' is a string while the 4 logged in the console is a number.
+*/
+
+// MY CODE
+//code to output 10
+
+const value = 3
+console.log(value + 4 + value)
---
+++ IWA7/Challenge-2/script.js
@@ -0,0 +1,10 @@
+// TEMPLATE
+/*
+const nickname= "Timmy";
+const firstname = "Timothy";
+
+console.log("Good Morning, ${nickname} || {firstname}!")
+*/
+
+// MY CODE
+
