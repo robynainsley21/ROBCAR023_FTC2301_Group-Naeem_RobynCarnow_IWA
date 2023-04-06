a7bc83cfec685907dfc96b6600aacd0ca8c61b9d
robynainsley21 <robynainsley2i@gmail.com>
2023-04-06 14:59:13 +0200

    Edited IWA8 script file

--- IWA8/Challenge-1/script.js
+++ IWA8/Challenge-1/script.js
@@ -80,7 +80,7 @@ const leo = {
 }
 
 const sarah = {
-	name: `${sarahName.trimEnd()} ${sarahSurname}`,
+	ame: `${sarahName.trimEnd()} ${sarahSurname}`,
 	age: 62,
 	accessId: '6b279ae5-5657-4240-80e9-23f6b635f7a8',
 	balance: sarahBalance,
