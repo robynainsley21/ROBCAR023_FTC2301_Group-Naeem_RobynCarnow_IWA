3cd396cdac702dc014ef9b2291d09e8c55890afb
robynainsley21 <robynainsley2i@gmail.com>
2023-04-29 13:46:49 +0200

    Edited script file

--- IWA16/Challenge-1/scripts.js
+++ IWA16/Challenge-1/scripts.js
@@ -123,113 +123,122 @@ why did the original code not work?
 
 */
 
-// scripts.js
 
 const MONTHS = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
 ]
-  
+
 const data = {
-    response: {
-      requestType: "FETCH_ATHLETE_DATA",
-      requestBy: "ALL_MATCHING_ATHLETES",
-      forDisplay: "BEST_RACES",
-  
-      data: {
-        NM372: {
-          firstName: "Nwabisa",
-          surname: "Masiko",
-          id: "NM372",
-          races: [
-            {
-              date: '2022-11-18T20:00:00.000Z',
-              time: [9, 7, 8, 6],
-            },
-            {
-              date: '2022-12-02T20:00:00.000Z',
-              time: [6, 7, 8, 7],
-            },
-          ],
-        },
-  
-        SV782: {
-          firstName: "Schalk",
-          surname: "Venter",
-          id: "SV782",
-          races: [
-            {
-              date: '2022-11-18T20:00:00.000Z',
-              time: [10, 8, 3, 12],
-            },
-            {
-              date: '2022-11-25T20:00:00.000Z',
-              time: [6, 8, 9, 11],
-            },
-            {
-              date: '2022-12-02T20:00:00.000Z',
-              time: [10, 11, 4, 8],
-            },
-            {
-              date: '2022-12-09T20:00:00.000Z',
-              time: [9, 8, 9, 11],
-            },
-          ],
-        },
+  response: {
+    requestType: "FETCH_ATHLETE_DATA",
+    requestBy: "ALL_MATCHING_ATHLETES",
+    forDisplay: "BEST_RACES",
+
+    data: {
+      NM372: {
+        firstName: "Nwabisa",
+        surname: "Masiko",
+        id: "NM372",
+        races: [
+          {
+            date: '2022-11-18T20:00:00.000Z',
+            time: [9, 7, 8, 6],
+          },
+          {
+            date: '2022-12-02T20:00:00.000Z',
+            time: [6, 7, 8, 7],
+          },
+        ],
+      },
+
+      SV782: {
+        firstName: "Schalk",
+        surname: "Venter",
+        id: "SV782",
+        races: [
+          {
+            date: '2022-11-18T20:00:00.000Z',
+            time: [10, 8, 3, 12],
+          },
+          {
+            date: '2022-11-25T20:00:00.000Z',
+            time: [6, 8, 9, 11],
+          },
+          {
+            date: '2022-12-02T20:00:00.000Z',
+            time: [10, 11, 4, 8],
+          },
+          {
+            date: '2022-12-09T20:00:00.000Z',
+            time: [9, 8, 9, 11],
+          },
+        ],
       },
     },
+  },
 };
-  
-  // Only edit below this comment
-  
+
+// Only edit below this comment
+
 const createHtml = (athlete) => {
-    firstName, surname, id, races = athlete
-    [date], [time] = races.reverse() /* find true method for reverse() */
-  
-    const fragment = document.createDocumentFragment(); /* creates a new document */
-        console.log(fragment)
-  
-    const title = document.createElement(h2); /* creates element specified */
-    title.innerHTML = data.data ;
-    fragment.appendChild(title); /* returns copy of node */
-      
-    const day = date.getDate();
-    const month = MONTHS[date.month];
-    const year = date.year;
-
-    const first = tobechanged
-  
-    first, second, third, fourth = timeAsArray;
-    const total = first + second + third + fourth;
-  
-    const hours = total / 60;
-    const minutes = total / hours / 60;
-  
-
-    // list.innerHTML =  putsomethinghere /* html */   
-  
-    const nwabisaId = document.getElementBy('[data-athlete="NM372"]')
-        console.log(nwabisaId)
-    // const nwabisa = ''
+  //should i make variables for these?
+  // athlete.firstName, athlete.surname, athlete.id, athlete['races'] = athlete
+  // [athlete[races][date]], [athlete[races][time]] = races.reverse()
 
 
+  const fragment = document.createDocumentFragment(); //not part of the main DOM tree; 
+  //creates doc fragment, appends elements to that fragment and then appends the doc to the DOM tree
+  //the doc frag is replaced by all its children
 
-    fragment.appendChild(list);
+  const title = document.createElement('h2');
+  title.innerHTML = athlete.id;
+  fragment.appendChild(title); //appends element (title) as the last child of another (fragment)
+
+
+  // const list = document.createElement('dl');
+
+  // const day = date.getDate();
+  // const month = MONTHS[date.month];
+  // const year = date.year;
+
+  // first, second, third, fourth = timeAsArray;
+  // total = first + second + third + fourth;
+
+  // const hours = total / 60;
+  // const minutes = total / hours / 60;
+
+
+  //   list.innerHTML = /* html */ `
+  //   <dt>Athlete</dt>
+  //   <dd>${firstName surname}</dd>
+
+  //   <dt>Total Races</dt>
+  //   <dd>${races}</dd>
+
+  //   <dt>Event Date (Latest)</dt>
+  //   <dd>${day month year}</dd>
+
+  //   <dt>Total Time (Latest)</dt>
+  //   <dd>${hours.padStart(2, 0) minutes}</dd>
+  // `;
+
+  // fragment.appendChild(list);
 }
-  
-  [NM372], [SV782] = data
-const nwabisa = document.querySelector(NM372).appendChild(createHtml(NM372));
-const schalk = document.querySelector(SV782).appendChild(createHtml(SV782));
 
-createHtml(nwabisa)
-createHtml(schalk)
+createHtml(data['response']['data']['NM372'])
+console.log(data['response']['data']['NM372'])
+
+
+document.querySelector('NM372').appendChild(createHtml('NM372'));
+document.querySelector('SV782').appendChild(createHtml('SV782'));
